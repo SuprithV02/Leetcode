4c2d81108d1a8fc3028e2b259413618a498c65bc
SuprithV02 <100951748+SuprithV02@users.noreply.github.com>
2026-08-16 18:13:31 +0530

    Time: 2 ms (99.65%), Space: 44.8 MB (53.62%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,19 +1,37 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         
-        if(s.length() != t.length()) return false;
+        // if(s.length() != t.length()) return false;
 
-        int[] freq = new int[26];
+        // char[] s1 = s.toCharArray();
+        // char[] t1 = t.toCharArray();
 
-        for(int i = 0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            freq[ch - 'a']++;
+        // Arrays.sort(s1);
+        // Arrays.sort(t1);
+
+        // int i = 0;
+
+        // while(i < s1.length){
+        //     if(s1[i] != t1[i]) return false;
+
+        //     i++;
+        // }
+
+        // return true;
+
+        int count[] = new int[26];
+
+        if(s.length() != t.length()){
+            return false;
         }
 
-        for(int j = 0; j<t.length(); j++){
-            char ch = t.charAt(j);
-            freq[ch - 'a']--;
-            if(freq[ch - 'a'] < 0) return false;
+        for(char ch : s.toCharArray()){
+            count[ch - 'a']++;
+        }
+
+        for(char ch : t.toCharArray()){
+            count[ch - 'a']--;
+            if(count[ch - 'a'] < 0) return false;
         }
 
         return true;
